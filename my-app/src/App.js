06d5a95100b0e114496ea9716d9eb33c1020b717
Karolina Krysiak <71433414+KarolinaKrysiak@users.noreply.github.com>
2023-03-14 21:36:57 +0100

    Added comments

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,25 +2,30 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-	const [tasks, setTasks] = useState([]);
-	const [newTask, setNewTask] = useState("");
+	const [tasks, setTasks] = useState([]); // state variable for tasks and a function to update it
+	const [newTask, setNewTask] = useState(""); // state variable for the new task and a function to update it
 
+	// input change
 	const handleInputChange = (e) => {
 		setNewTask(e.target.value);
 	};
 
+	// form submission
 	const handleSubmit = (e) => {
-		e.preventDefault();
+		e.preventDefault(); // Prevent the default form submission behavior
+
+		// check if the new task is not an empty string after trimming whitespace
 		if (newTask.trim() !== "") {
-			// check if newTask is not empty after trimming whitespace
-			setTasks([...tasks, newTask]);
-			setNewTask("");
+			setTasks([...tasks, newTask]); // Add the new task to the tasks array
+			setNewTask(""); // Clear the new task state variable
 		} else {
 			alert("Please enter a task.");
 		}
 	};
 
+	// Define a function to handle key down events on the input
 	const handleKeyDown = (e) => {
+		// Check if the key pressed is the Enter key
 		if (e.key === "Enter") {
 			e.preventDefault();
 			handleSubmit(e);
@@ -28,7 +33,7 @@ function App() {
 	};
 
 	const handleDelete = (index) => {
-		setTasks(tasks.filter((task, i) => i !== index));
+		setTasks(tasks.filter((task, i) => i !== index)); // Remove the task
 	};
 
 	return (
@@ -41,11 +46,12 @@ function App() {
 					value={newTask}
 					onChange={handleInputChange}
 					onKeyDown={handleKeyDown}
-				/>
+				/>{" "}
 				<button type="submit">Add</button>
 			</form>
 			<ul>
 				{tasks.map((task, index) => (
+					// map over the tasks array to create list items for each task
 					<li key={index}>
 						{index + 1}. {task}
 						<button onClick={() => handleDelete(index)}>Delete</button>
