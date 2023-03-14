3f13807d09a0701b932d483531e2205d2f505fce
Karolina Krysiak <71433414+KarolinaKrysiak@users.noreply.github.com>
2023-03-14 21:12:17 +0100

    Fixed conflicts from wrong updates

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,23 +2,21 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-	// declare state variables for tasks and new task
 	const [tasks, setTasks] = useState([]);
 	const [newTask, setNewTask] = useState("");
 
-	// update new task state based on input change
 	const handleInputChange = (e) => {
 		setNewTask(e.target.value);
 	};
 
-	// add new task to tasks array when form is submitted
 	const handleSubmit = (e) => {
-		e.preventDefault(); // prevent default form submission behavior
+		e.preventDefault();
 		if (newTask.trim() !== "") {
-			setTasks([...tasks, newTask]); // add new task to tasks array
-			setNewTask(""); // reset new task state to empty string
+			// check if newTask is not empty after trimming whitespace
+			setTasks([...tasks, newTask]);
+			setNewTask("");
 		} else {
-			alert("Please enter a task."); // show alert if task is empty
+			alert("Please enter a task.");
 		}
 	};
 
@@ -29,27 +27,10 @@ function App() {
 		}
 	};
 
-	// remove a task from tasks array
 	const handleDelete = (index) => {
 		setTasks(tasks.filter((task, i) => i !== index));
 	};
 
-	const handleDragStart = (e, index) => {
-		e.dataTransfer.setData("text/plain", index);
-	};
-
-	const handleDragOver = (e, index) => {
-		e.preventDefault();
-		const draggedIndex = e.dataTransfer.getData("text/plain");
-		if (draggedIndex !== index) {
-			const newTasks = [...tasks];
-			const [removed] = newTasks.splice(draggedIndex, 1);
-			newTasks.splice(index, 0, removed);
-			setTasks(newTasks);
-		}
-	};
-
-	// return JSX for rendering to the DOM
 	return (
 		<div className="App">
 			<h1>To-Do List</h1>
@@ -64,14 +45,8 @@ function App() {
 				<button type="submit">Add</button>
 			</form>
 			<ul>
-				{/* map over tasks array to render each task as a list item */}
 				{tasks.map((task, index) => (
-					<li
-						key={index}
-						draggable="true"
-						onDragStart={(e) => handleDragStart(e, index)}
-						onDragOver={(e) => handleDragOver(e, index)}
-					>
+					<li key={index}>
 						{index + 1}. {task}
 						<button onClick={() => handleDelete(index)}>Delete</button>
 					</li>
