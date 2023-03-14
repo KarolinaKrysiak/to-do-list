7be31b3ab2d3984fa312f86bbe01a8624c0ca1f5
Karolina Krysiak <71433414+KarolinaKrysiak@users.noreply.github.com>
2023-03-14 11:50:31 +0100

    App functionality

---
+++ my-app/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+function App() {
+	const [tasks, setTasks] = useState([]);
+	const [newTask, setNewTask] = useState("");
+
+	const handleInputChange = (e) => {
+		setNewTask(e.target.value);
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setTasks([...tasks, newTask]);
+		setNewTask("");
+	};
+
+	const handleDelete = (index) => {
+		setTasks(tasks.filter((task, i) => i !== index));
+	};
+
+	return (
+		<div className="App">
+			<h1>To-Do List</h1>
+			<form onSubmit={handleSubmit}>
+				<input type="text" placeholder="Add new task" value={newTask} onChange={handleInputChange} />
+				<button type="submit">Add</button>
+			</form>
+			<ul>
+				{tasks.map((task, index) => (
+					<li key={index}>
+						{task}
+						<button onClick={() => handleDelete(index)}>Delete</button>
+					</li>
+				))}
+			</ul>
+		</div>
+	);
+}
+
+export default App;
